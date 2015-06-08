1931bce0430dcde0ef53d011e0f73064f7c5e166
Kerri Shotts <kerrishotts@gmail.com>
2015-06-07 23:02:36 -0500

    Theme Manager
    Basic first pass at the theme manager for the app. This will let us
    define multiple themes and switch between them at will. This is a
    departure from prior apps where the theme was based on the platform and
    platform only. This can still be the case, but the theme itself isn’t
    tied to the platform.

---
+++ logology-v05/src/www/js/lib/ThemeManager.js
@@ -0,0 +1,43 @@
+/* @flow */
+"use strict";
+
+import Emitter from "yasmf-emitter";
+
+const _current = Symbol(),
+      _available = Symbol();
+
+export default class ThemeManager extends Emitter {
+
+    constructor() {
+        super();
+        this[_current] = null;
+        this[_available] = new Set();
+    }
+
+    registerTheme(theme/*: Theme*/)/*: void*/ {
+        this[_available].add(theme);
+    }
+
+    get availableThemes()/*: Array*/ {
+        return Array.from(this[_available]);
+    }
+
+    get currentTheme()/*: Theme*/ {
+        return this[_current];
+    }
+    set currentTheme(v/*: Theme*/) {
+        let bodyEl = document.body;
+        if (this.currentTheme) {
+            bodyEl.classList.remove(this[_current].cssClass);
+        }
+        this[_current] = v;
+        bodyEl.classList.add(v.cssClass);
+        this.emit("themeChanged", v);
+    }
+
+    destroy()/*: void*/ {
+        this[_current] = null;
+        this[_available] = null;
+        super.destroy();
+    }
+}
