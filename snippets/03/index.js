ccebe39357847f5a7f34f78e01da7cee0c0faa06
Kerri Shotts <kerrishotts@gmail.com>
2015-11-08 17:34:49 -0600

    Add more snippets to ch3

--- snippets/03/index.js
+++ snippets/03/index.js
@@ -3,5 +3,10 @@ module.exports = [
   { name: "Example 1: Calculation",                         base: "03/ex1-calculation", layout: "scss,css", scss: true },
   { name: "Example 2a: Variables: Vanilla CSS; No Cascade", base: "03/ex2-variables/a", layout: "scss,html", html: true, scss: true },
   { name: "Example 2b: Variables: Vanilla CSS; Cascade",    base: "03/ex2-variables/b", layout: "scss,html", html: true, scss: true },
-  { name: "Example 2c: Variables: Sassy CSS",               base: "03/ex2-variables/c", layout: "scss,html", html: true, scss: true }
+  { name: "Example 2c: Variables: Sassy CSS",               base: "03/ex2-variables/c", layout: "scss,html", html: true, scss: true },
+  { name: "Example 3: Nesting",                             base: "03/ex3-nesting",     layout: "scss,html", html: true, scss: true },
+  { name: "Example 4a: Mixins: Simple Mixin",               base: "03/ex4-mixins/a",    layout: "scss,css", scss: true },
+  { name: "Example 4b: Mixins: Variable Interpolation",     base: "03/ex4-mixins/b",    layout: "scss,css", scss: true },
+  { name: "Example 4c: Mixins: Looping",                    base: "03/ex4-mixins/c",    layout: "scss,css", scss: true },
+  { name: "Example 4d: Functions",                          base: "03/ex4-mixins/d",    layout: "scss,css", scss: true }
 ];
