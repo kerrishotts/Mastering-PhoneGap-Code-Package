c7570a874e298e6ed5f284cf2db703463af93676
Kerri Shotts <kerrishotts@gmail.com>
2015-11-08 17:33:45 -0600

    Update example names

--- snippets/05/index.js
+++ snippets/05/index.js
@@ -4,6 +4,5 @@ module.exports = [
   { name: "Example 2: Responsive Icons",             base: "04/ex2-responsive-icons",             layout: "scss,html", html: true, scss: true },
   { name: "Example 3: Responsive Header Images",     base: "04/ex3-responsive-header-images",     layout: "scss,html", html: true, scss: true },
   { name: "Example 4: Responsive Fullscreen Images", base: "04/ex4-responsive-fullscreen-images", layout: "scss,html", html: true, scss: true },*/
-  { name: "Example 5a: Accessible UI",               base: "05/ex5-accessible-ui/a",              layout: "scss,html", html: true, scss: true },
-  { name: "Example 5b: Accessible UI",               base: "05/ex5-accessible-ui/b",              layout: "es6,scss,html", js: true, html: true, scss: true }
+  { name: "Example 1: Accessible UI",               base: "05/ex1-accessible-ui/b",              layout: "es6,scss,html", js: true, html: true, scss: true }
 ];
