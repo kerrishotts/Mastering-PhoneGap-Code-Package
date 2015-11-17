ec9af49ba52cdaab51cd461fcf3d46a64bbe2947
Kerri Shotts <kerrishotts@gmail.com>
2015-11-17 17:28:30 -0600

    Settings is now a singleton

--- logology-v12/src/www/js/app/index.js
+++ logology-v12/src/www/js/app/index.js
@@ -10,15 +10,17 @@
 
 /*globals MobileAccessibility, setImmediate*/
 
+// we need to use the Fetch API (replaces XHR), but not available in all
+// instances, so we use a polyfill
 require("whatwg-fetch");
 
+// Need ES6 polyfill as well
 import "babel/polyfill";
-
-import h from "yasmf-h";
 
 import once from "once";
 
 import Emitter from "yasmf-emitter";
+
 import GCS from "$LIB/grandCentralStation";
 import {createSoftKeyboard} from "$LIB/SoftKeyboard";
 import L from "./localization/localization";
@@ -29,53 +31,13 @@ import {createThemeManager} from "$LIB/ThemeManager";
 import {createNavigationViewController} from "$LIB/NavigationViewController";
 import {createSplitViewController} from "$LIB/SplitViewController";
 
-import {createSettings} from "$MODELS/Settings";
+import {settings} from "$MODELS/Settings";
 import {createDictionaries} from "$MODELS/Dictionaries";
 
 import StarterDictionary from "$MODELS/StarterDictionary";
 import XHRDictionary from "$MODELS/XHRDictionary";
 
 let SVGInjector = require("svg-injector");
-/*
-function simpleAlert() {
-    let outerDiv = document.createElement("div"),
-        innerDiv = document.createElement("div"),
-        titleEl  = document.createElement("h1"),
-        messageEl =  document.createElement("p"),
-        buttonEl = document.createElement("button"),
-        mainWindow = document.getElementById("mainWindow");
-    outerDiv.setAttribute("is", "y-alert-container");
-    innerDiv.setAttribute("is", "y-alert-dialog");
-    titleEl.textContent = "Important!";
-    messageEl.textContent = "This is an important message. Close this by tapping OK.";
-    buttonEl.textContent = "OK";
-
-    // accessibility
-    innerDiv.setAttribute("role", "alertdialog");
-    messageEl.setAttribute("role", "alert");
-
-    // create the dom structure
-    outerDiv.appendChild(innerDiv);
-    innerDiv.appendChild(titleEl);
-    innerDiv.appendChild(messageEl);
-    innerDiv.appendChild(buttonEl);
-
-    document.body.appendChild(outerDiv);
-    setTimeout(function () {
-      outerDiv.classList.add("visible");
-      mainWindow.setAttribute("aria-hidden", true);
-      buttonEl.focus();
-    }, 0);
-
-    buttonEl.addEventListener("click", function() {
-        outerDiv.classList.remove("visible");
-        setTimeout(function() {
-          mainWindow.removeAttribute("aria-hidden");
-          document.body.removeChild(outerDiv);
-        }, 400);
-    }, false);
-}
-*/
 
 import {createSearchViewController} from "$CONTROLLERS/SearchViewController";
 import {createMenuViewController} from "$CONTROLLERS/MenuViewController";
@@ -154,7 +116,7 @@ class App extends Emitter {
             await this.configurei18n();
 
             // load settings
-            this.settings = createSettings();
+            this.settings = settings;
 
             // create dictionaries list
             this.dictionaries = createDictionaries();
