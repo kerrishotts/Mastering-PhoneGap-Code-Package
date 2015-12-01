0c16fef0e5fb99382aed3a0207f1763d6bbc8785
Kerri Shotts <kerrishotts@gmail.com>
2015-12-01 00:38:08 -0600

    Add last dictionary; a way to get entries as a model; switch to a singleton

--- logology-v12/src/www/js/app/models/Settings.js
+++ logology-v12/src/www/js/app/models/Settings.js
@@ -3,12 +3,13 @@ import Emitter from "yasmf-emitter";
 import GCS from "../../lib/grandCentralStation";
 
 // private property symbols
-let _fontFamily = Symbol(), // font family
-    _fontSize = Symbol(),   // 0 = use system default, otherwise specific point sizes
-    _theme = Symbol(),      // light, dark, lightHighContrast, etc.
-    _externalResources = Symbol(), // link templates to external resources, like Wikipedia
-    _showImages = Symbol(), // determines if images are shown (if possible)
-    _pageSize = Symbol();   // page length (search results, etc.)
+let _fontFamily = Symbol("fontFamily"), // font family
+    _fontSize = Symbol("fontSize"),   // 0 = use system default, otherwise specific point sizes
+    _theme = Symbol("theme"),      // light, dark, lightHighContrast, etc.
+    _externalResources = Symbol("externalResources"), // link templates to external resources, like Wikipedia
+    _showImages = Symbol("images"), // determines if images are shown (if possible)
+    _lastDictionary = Symbol("lastDictionary"), // records the last dictionary
+    _pageSize = Symbol("pageSize");   // page length (search results, etc.)
 
 export default class Settings extends Emitter {
     constructor() {
@@ -18,6 +19,7 @@ export default class Settings extends Emitter {
         this[_theme] = "light";
         this[_showImages] = true;
         this[_pageSize] = 100;
+        this[_lastDictionary] = undefined; // if undefined, the app will pick the first one
         this[_externalResources] = {
             "Wikipedia": "http://www.wikipedia.org/search-redirect.php?language=en&search=%WORD%",
             "WordNet": "http://wordnetweb.princeton.edu/perl/webwn?s=%WORD%"
@@ -34,6 +36,41 @@ export default class Settings extends Emitter {
         this.emit("settingsChanged");
     }
 
+    get entries() {
+        return [
+            {name: "setting:font-family", key: "fontFamily", value: this.fontFamily, type: "select", options: [
+                {name: "setting:font-family:Default", value: undefined},
+                {name: "setting:font-family:Helvetica-Neue", value: "HelveticaNeue, 'Helvetica Neue', Helvetica, Arial, sans-serif"},
+                {name: "setting:font-family:Lucida-Grande", value: "'Lucida Sans Unicode', 'Lucida Grande', sans-serif"},
+                {name: "setting:font-family:Georgia", value: "Georgia, serif"},
+                {name: "setting:font-family:Palatino", value: "'Palatino Linotype', 'Book Antiqua', Palatino, serif"},
+                {name: "setting:font-family:Times-New-Roman", value: "'Times New Roman', Times, serif"}
+            ]},
+            {name: "setting:font-size",   key: "fontSize", value: this.fontSize, type: "select", options: [
+                {name: "setting:font-size:Default", value: 0},
+                {name: "setting:font-size:Tiny", value: 50},
+                {name: "setting:font-size:Small", value: 75},
+                {name: "setting:font-size:Normal", value: 100},
+                {name: "setting:font-size:Large", value: 125},
+                {name: "setting:font-size:Huge", value: 150},
+                {name: "setting:font-size:Gigantic", value: 250}
+            ]},
+            {name: "setting:theme", key: "theme", value: this.theme, type: "select", options: [
+                {name: "setting:theme:Default", value: "default"},
+                {name: "setting:theme:Light", value: "light"},
+                {name: "setting:theme:Dark", value: "dark"}
+            ]},
+            {name: "setting:page-size", key: "pageSize", value: this.pageSize, type: "select", options: [
+                {name: "setting:page-size:20", value: 25},
+                {name: "setting:page-size:50", value: 50},
+                {name: "setting:page-size:80", value: 80},
+                {name: "setting:page-size:100", value: 100},
+                {name: "setting:page-size:150", value: 150},
+                {name: "setting:page-size:200", value: 200}
+            ]}
+        ]
+    }
+
     retrieveSettings() {
         if (localStorage.fontFamily !== undefined) { this[_fontFamily] = localStorage.fontFamily; }
         if (localStorage.fontSize !== undefined) { this[_fontSize] = localStorage.fontSize; }
@@ -41,6 +78,7 @@ export default class Settings extends Emitter {
         if (localStorage.showImages !== undefined) { this[_showImages] = localStorage.showImages; }
         if (localStorage.externalResources !== undefined) { this[_externalResources] = JSON.parse(localStorage.externalResources); }
         if (localStorage.pageSize !== undefined) { this[_pageSize] = localStorage.pageSize; }
+        if (localStorage.lastDictionary !== undefined) { this[_lastDictionary] = localStorage.lastDictionary; }
         this.emit("settingsRetrieved");
         GCS.emit("APP:SETTINGS:loaded");
         this.emit("settingsChanged"); // this will save the settings again, but that's OK
@@ -53,6 +91,7 @@ export default class Settings extends Emitter {
         if (this.showImages !== undefined) { localStorage.showImages = this.showImages; }
         localStorage.externalResources = JSON.stringify(this.externalResources);
         if (this.pageSize !== undefined) { localStorage.pageSize = this.pageSize; }
+        if (this.lastDictionary !== undefined) { localStorage.lastDictionary = this.lastDictionary; }
         this.emit("settingsPersisted");
         GCS.emit("APP:SETTINGS:persisted");
     }
@@ -93,6 +132,12 @@ export default class Settings extends Emitter {
         this[_pageSize] = s;
     }
 
+    get lastDictionary() { return this[_lastDictionary]; }
+    set lastDictionary(d) {
+        this._emitChangeNotice("lastDictionary", d, this[_lastDictionary]);
+        this[_lastDictionary] = d;
+    }
+
     get externalResources() { return this[_externalResources]; }
 
     addExternalResource({name, template}) {
@@ -105,6 +150,7 @@ export default class Settings extends Emitter {
 }
 
 export let settings = new Settings();
+settings.retrieveSettings();
 
 export function createSettings(...args) {
     return new Settings(...args);
