9c80148fd7d0846e5643c88311dc80d1837b3e0c
Kerri Shotts <kerrishotts@gmail.com>
2015-12-01 23:56:19 -0600

    guess to see if my thought process is right

--- logology-v12/src/www/js/lib/SoftKeyboard.js
+++ logology-v12/src/www/js/lib/SoftKeyboard.js
@@ -207,7 +207,7 @@ export default class SoftKeyboard extends Emitter {
         setTimeout(() => {
             let screenHeight = window.innerHeight; //(document.body.clientWidth === window.screen.height ? window.screen.width : window.screen.height);
             let scrollContainers = getScrollContainers(this.selectorString);
-            let keyboardHeight = e.keyboardHeight;
+            let keyboardHeight = e.keyboardHeight / devicePixelRatio;
 
             if (this.showElementUnderKeyboard) {
                 showElementUnderKeyboard(keyboardHeight);
