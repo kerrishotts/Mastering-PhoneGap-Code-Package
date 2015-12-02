e31348e5b92fc12364157ec073fe6208abd3fc13
Kerri Shotts <kerrishotts@gmail.com>
2015-12-01 23:58:09 -0600

    indexed db key value store using treo
    NOTE: Couldn’t use tree-promise because it interfered with
    chai-as-promised in tests. Probably would have worked just fine in the
    browser, but for now I’ll stick with the callback version.

---
+++ logology-v12/src/www/js/app/lib/IndexedDBKVStore.js
@@ -0,0 +1,96 @@
+"use strict";
+
+import Emitter from "yasmf-emitter";
+import treo from "treo";
+import treoWebsql from "treo-websql";
+
+//region private properties
+const _namespace = Symbol("_namespace"),
+      _indexedDB = Symbol("_indexedDB"),
+      _db = Symbol("_db");
+//endregion
+
+const kvSchema = treo.schema()
+                     .version(1)
+                       .addStore("kv");
+
+export default class IndexedDBKVStore extends Emitter {
+    constructor({namespace = "_default", indexedDB} = {}) {
+        super();
+        this[_namespace] = namespace;
+        this[_indexedDB] = indexedDB;
+        if (indexedDB) {
+            global.indexedDB = indexedDB;
+        }
+        this[_db] = treo(namespace, kvSchema);
+        if (!indexedDB) {
+            this.db.use(treoWebsql());
+        }
+    }
+
+    get db() {
+        return this[_db];
+    }
+
+    get namespace() {
+        return this[_namespace];
+    }
+
+    set(key, value) {
+        let store = this.db.store("kv");
+        return new Promise((resolve, reject) => {
+            store.put(key, value, (err, data) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(data);
+                    this.emit(`model:changed:${key}`, value);
+                }
+            });
+        });
+    }
+
+    get(key, defaultValue) {
+        let store = this.db.store("kv");
+        return new Promise((resolve, reject) => {
+            store.get(key, (err, data) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(data === null || data === undefined ? defaultValue : data);
+                }
+            });
+        });
+    }
+
+    exists(key) {
+        let store = this.db.store("kv");
+        return new Promise((resolve, reject) => {
+            store.get(key, (err, data) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(!(data === null || data === undefined));
+                }
+            });
+        });
+    }
+
+    remove(key) {
+        let store = this.db.store("kv");
+        return new Promise((resolve, reject) => {
+            store.del(key, (err, data) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(data);
+                    this.emit(`model:changed:${key}`);
+                }
+            });
+        });
+    }
+}
+
+export function createIndexedDBKVStore(options = {}) {
+    return new IndexedDBKVStore(options);
+}
