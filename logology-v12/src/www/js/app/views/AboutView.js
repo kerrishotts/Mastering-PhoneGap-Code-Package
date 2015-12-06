f0a34417a09dc220109daa99124c056cefd43f36
Kerri Shotts <kerrishotts@gmail.com>
2015-12-06 00:04:54 -0600

    Add book cover; word net license

--- logology-v12/src/www/js/app/views/AboutView.js
+++ logology-v12/src/www/js/app/views/AboutView.js
@@ -40,24 +40,26 @@ export default class AboutView extends View {
 
     template() {
         let model = kp.get(this, "controller.model");
-        //if (!model) {model = {dictionaries: []}};
         return scrollContainer({contents: [
             textContainer({contents: [
-                h.p(`Logology is an app about the love of words.
-                    Combinations of words are our thoughts, crystalized,
-                    ready to put into action. Without definitions, our
-                    thoughts would be immaterial`),
-                h.p([h.span("Logology was created as a demonstration app for the book, "),
-                    h.a("Mastering PhoneGap Mobile Application Development", {
+                h.img({
+                    attrs:{
+                        srcset: "img/book-cover-xhidpi.jpg 2500w, img/book-cover-hidpi.jpg 1250w, img/book-cover-lodpi.jpg 625w",
+                        src:"img/book-cover-lodpi.jpg"
+                    },
+                    styles: {
+                        width: "100%"
+                    }
+                }),
+                h.p(L.T("about:p:1")),
+                h.p(h.a(L.T("about:book-title"), {
                         attrs: {
                             href: "javascript: return false;",
                             link: "https://www.packtpub.com/application-development/mastering-phonegap-mobile-application-development"
                         }
-                    }),
-                    h.span(`, and was built to illustrate how to create mobile hybrid
-                            apps using Cordova and PhoneGap.`)]),
-                h.p(`This app relies upon the following technologies. Tap each one
-                    for additional information, including licensing information.`)
+                    })),
+                h.p(L.T("about:p:2")),
+                h.p(L.T("about:p:3"))
             ]}),
             list({
                 contents: ["plugin", "package", "dev-package"].map ( t=> {
@@ -76,6 +78,16 @@ export default class AboutView extends View {
                         })
                     );
                 })
+            }),
+            textContainer({
+                contents: [
+                    h.h1(L.T("about:wordnet")),
+                    h.p(L.T("about:wordnet:use")),
+                    h.p(L.T("about:wordnet:license:1")),
+                    h.p(L.T("about:wordnet:license:2")),
+                    h.p(L.T("about:wordnet:license:3")),
+                    h.p(L.T("about:wordnet:license:4")),
+                ]
             })
         ]});
     }
