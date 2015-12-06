ca0d0d70d604d2e4879c75781767c3665c3fe1f4
Kerri Shotts <kerrishotts@gmail.com>
2015-12-06 00:08:29 -0600

    notes

---
+++ logology-v12/src/www/js/app/controllers/NotesViewController.js
@@ -0,0 +1,57 @@
+"use strict";
+
+import GCS from "$LIB/grandCentralStation";
+import ViewController from "$LIB/ViewController";
+import {createNotesView} from "$VIEWS/NotesView";
+import h from "yasmf-h";
+import el from "$LIB/templates/el";
+import navigationBar from "$WIDGETS/bars/navigation";
+import widgetGroup from "$WIDGETS/group";
+import glyph from "$WIDGETS/glyph";
+import lemmaActions from "$VIEWS/lemmaActions";
+import L from "$APP/localization/localization";
+
+let _lemma = Symbol("_lemma"),
+    _dictionary = Symbol("_dictionary");
+
+export default class NotesViewController extends ViewController {
+    constructor({model}={}) {
+        super({title: "Note", model, view: createNotesView()});
+    }
+    get TARGET_SELECTORS() {
+        return [
+            {selector: "tap:.back-icon", emit: "backTapped"},
+            {selector: "tap:.trash-icon", emit: "onTrashTapped"}
+        ];
+    }
+    onBackTapped() {
+        GCS.emit("APP:DO:back");
+    }
+    onTrashTapped() {
+        // delete note from word
+    }
+    onDidRemoveFromParent() {
+        this.destroy();
+    }
+    template() {
+        return h.el("main.NotesViewController y-container?is=y-definition-view-controller", [
+            navigationBar({contents:[
+                widgetGroup({contents:[
+                    glyph({icon:"back", contents: L.T("icon:back"), title: L.T("general:tap-to-go-back")})
+                ]}),
+                widgetGroup({contents:[
+                    h.el("h1?is=y-title", L.T("title:notes"))
+                ], flex: true}),
+                widgetGroup({contents: [
+                    glyph({icon:"trash", title: L.T("actions:delete-note:title"),
+                           contents: L.T("actions:delete-note")})
+                ], align:"right"})
+            ]}),
+            this.renderSubviews()
+        ]);
+    }
+}
+
+export function createNotesViewController(options = {}) {
+    return new NotesViewController(options);
+}
