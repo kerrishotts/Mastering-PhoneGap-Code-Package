8bb574070e2a92b231e06d6821c4a31300eaf0f0
Kerri Shotts <kerrishotts@gmail.com>
2015-12-06 00:04:24 -0600

    Add favorite status

--- logology-v12/src/www/js/app/views/lemmaActions.js
+++ logology-v12/src/www/js/app/views/lemmaActions.js
@@ -4,8 +4,12 @@ import L from "$APP/localization/localization";
 
 import glyph from "$WIDGETS/glyph";
 
-export default function lemmaActions() {
-    return [ glyph({icon:"fav",   title: L.T("actions:favorite:title"), contents: L.T("actions:favorite")}),
+export default function lemmaActions({isFavorite = false, hasNote = false} = {}) {
+    return [ glyph({icon:"fav", contents: L.T(isFavorite ? "actions:unfavorite"
+                                                         : "actions:favorite"),
+                   props: { attrs: {"class": "fav-icon", "data-fav": isFavorite ? "yes" : "no",
+                                    title: L.T(isFavorite ? "actions:unfavorite:title"
+                                                          : "actions:favorite:title") } } }),
              glyph({icon:"share", title: L.T("actions:share:title"),    contents: L.T("actions:share")}),
              glyph({icon:"note",  title: L.T("actions:note:title"),     contents: L.T("actions:note")})];
 }
