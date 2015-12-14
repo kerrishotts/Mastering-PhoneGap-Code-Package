7bbcf62778409e96113f42dee40cd23e98f00d06
Kerri Shotts <kerrishotts@gmail.com>
2015-12-14 01:04:40 -0600

    update config

--- logology-v01/gulp/config.js
+++ logology-v01/gulp/config.js
@@ -12,26 +12,16 @@ var config = {
                 {src: "www/*.*",            dest: "www"},
                 {src: "www/html/**/*",      dest: "www/html"},
                 {src: "www/img/**/*",       dest: "www/img"},
-                //{src: "www/js/lib/**/*",    dest: "www/js/lib"},
-                {src: "res/**/*",           dest: "res"},
-                {src: "../node_modules/open-iconic/sprite/sprite.svg",
-                dest: "www/img/open-iconic"}
+                {src: "www/js/lib/**/*",    dest: "www/js/lib"},
+                {src: "res/**/*",           dest: "res"}
               ],
-        code: {src: "www/js/app/index.js", dest: "www/js/app"}
+        code: {src: "www/js/app/**/*.js",      dest: "www/js/app"}
     },
     lint: "src/www/js/app/**/*.js",
     "code-style": "src/www/js/app/**/*.js",
     serve: "build/www",
     watch: ["src/www/**/*"],
-    reload: ["src/www/**/*.js"],
-    aliases: {
-        "$APP": "src/www/js/app",
-        "$LIB": "src/www/js/lib",
-        "$WIDGETS": "src/www/js/lib/templates/widgets",
-        "$MODELS": "src/www/js/app/models",
-        "$VIEWS": "src/www/js/app/views",
-        "$CONTROLLERS": "src/www/js/app/controllers"
-    }
+    reload: ["src/www/**/*.js"]
 }
 
 module.exports = config;
