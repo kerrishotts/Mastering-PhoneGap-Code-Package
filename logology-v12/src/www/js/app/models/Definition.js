d1dd17283dbf9b472d9ea1a00adfa86c13b74286
Kerri Shotts <kerrishotts@gmail.com>
2015-12-16 00:20:44 -0600

    Automatically split lemmas from SQLDictionary

--- logology-v12/src/www/js/app/models/Definition.js
+++ logology-v12/src/www/js/app/models/Definition.js
@@ -1,7 +1,7 @@
 "use strict";
 export default class Definition {
     constructor({wordNetRef, lemmas=[], partOfSpeech, gloss } = {}) {
-        this.lemmas = lemmas;
+        this.lemmas = typeof lemmas === "string" ? lemmas.split(":::") : lemmas;
         this.partOfSpeech = partOfSpeech;
         this.gloss = gloss;
         this.wordNetRef = wordNetRef;
