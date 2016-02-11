88c93d30d626a20a999b1a188743db95f3da15e7
Kerri Shotts <kerrishotts@gmail.com>
2016-02-11 16:53:25 -0600

    testing profile

---
+++ logology-v12/test-ui/helpers/profiles/ios-simulator-9-2.js
@@ -0,0 +1,8 @@
+exports.profile = {
+    browserName: "",
+    platformName: "iOS",
+    platformVersion: "9.2",
+    deviceName: "iPhone Simulator",
+    nativeWebTap: true,
+    app: __dirname + "/../../../build/platforms/ios/build/emulator/Logology.app"
+}
