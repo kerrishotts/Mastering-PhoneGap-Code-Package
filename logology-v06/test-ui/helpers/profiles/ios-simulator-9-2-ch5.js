444b218fcd4d957011bb5c36f89497d7ac62377a
Kerri Shotts <kerrishotts@gmail.com>
2016-02-29 00:34:04 -0600

    still want to be able to test!

---
+++ logology-v06/test-ui/helpers/profiles/ios-simulator-9-2-ch5.js
@@ -0,0 +1,6 @@
+exports.profile = {
+    platformName: "iOS",
+    platformVersion: "9.2",
+    deviceName: "iPhone Simulator",
+    app: __dirname + "/../../../../logology-v05/build/platforms/ios/build/emulator/Logology.app"
+}
